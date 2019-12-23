409c9eaa75621caab175b2d7b2e708fbe4b72a52
Temperance <adulorier@gmail.com>
2019-12-23 07:30:57 +0100

    messaging done

--- lib/screens/Messaging/index.dart
+++ lib/screens/Messaging/index.dart
@@ -1,17 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:inclusive/screens/appdata.dart';
 import 'package:inclusive/theme.dart';
-
-class Message {
-  String text;
-  Message(this.text);
-}
-
-class Conversation {
-  List<Message> messages;
-  Conversation(this.messages);
-}
 
 class MessagingScreen extends StatefulWidget {
   @override
@@ -19,28 +11,84 @@ class MessagingScreen extends StatefulWidget {
 }
 
 class _MessagingState extends State<MessagingScreen> {
-  List<Conversation> conversations = [
-    Conversation([
-      Message('ok'),
-    ]),
-    Conversation([
-      Message('ko'),
-      Message('k'),
-    ])
+  String current;
+  List<String> conversations = [
+    'ea23JLKjsdjf',
+    'jsldjfkljezer',
+    'sdjkfjk',
+    'sdlkfjlkj',
+    'sdfkljlskdjf',
+    'jsldkjflk',
+    'jlskjdflkj',
+    'sjdlfjlkej',
+    'sjdflmkjmlk',
+    'sjdlfjs',
   ];
-  int current = 0;
-  String message = '';
 
-  List<Widget> buildConversationIcons() {
+  final ScrollController listScrollController = ScrollController();
+  final TextEditingController textController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    current = conversations[0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        buildConversationIcons(),
+        buildConversation(),
+        buildInput(),
+      ],
+    );
+  }
+
+  onSendMessage(String text) {
+    if (text.trim() != '') {
+      textController.clear();
+
+      String timestamp = DateTime.now().millisecondsSinceEpoch.toString();
+      var documentReference = Firestore.instance
+          .collection('messages')
+          .document(current)
+          .collection(current)
+          .document(timestamp);
+
+      Firestore.instance.runTransaction((transaction) async {
+        await transaction.set(
+          documentReference,
+          {
+            'idFrom': appData.identifier,
+            'timestamp': timestamp,
+            'content': text,
+          },
+        );
+      });
+      listScrollController.animateTo(0.0,
+          duration: Duration(milliseconds: 300), curve: Curves.easeOut);
+    } else {
+      Fluttertoast.showToast(msg: 'Nothing to send');
+    }
+  }
+
+  Widget buildConversationIcons() {
     List<Widget> conversationIcons = [];
 
     for (int i = 0; i < conversations.length; i++) {
       conversationIcons.add(GestureDetector(
         onTap: () {
-          setState(() => current = i);
+          setState(() {
+            current = conversations[i];
+            textController.clear();
+          });
         },
         child: Container(
-          padding: EdgeInsets.all(30),
+          decoration: BoxDecoration(
+              color: conversations[i] == current ? orange : Colors.transparent,
+              borderRadius: BorderRadius.circular(90.0)),
+          padding: EdgeInsets.all(20),
           child: SvgPicture.asset(
             'images/profile.svg',
             color: white,
@@ -49,30 +97,23 @@ class _MessagingState extends State<MessagingScreen> {
       ));
     }
 
-    return conversationIcons;
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Row(
+            children: conversationIcons,
+          )),
+    );
   }
 
-  List<Widget> buildConversation() {
-    List<Widget> conversation = [];
-    if (conversations.length == 0) {
-      return conversation;
-    }
-    Conversation currentConversation = conversations[current];
-    for (int i = 0; i < currentConversation.messages.length; i++) {
-      conversation.add(Text(currentConversation.messages[i].text));
-    }
-    return conversation;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    /* return Column(children: [Flexible(child: StreamBuilder(
+  Widget buildConversation() {
+    return StreamBuilder(
       stream: Firestore.instance
           .collection('messages')
-          //.document(groupChatId)
-          //.collection(groupChatId)
+          .document(current)
+          .collection(current)
           .orderBy('timestamp', descending: true)
-          .limit(20)
           .snapshots(),
       builder: (context, snapshot) {
         if (!snapshot.hasData) {
@@ -80,79 +121,45 @@ class _MessagingState extends State<MessagingScreen> {
               child: CircularProgressIndicator(
                   valueColor: AlwaysStoppedAnimation<Color>(orange)));
         } else {
-          return ListView.builder(
+          return Expanded(
+              child: ListView.builder(
+            shrinkWrap: true,
             itemBuilder: (context, index) =>
                 buildItem(index, snapshot.data.documents[index]),
             itemCount: snapshot.data.documents.length,
-          );
+            controller: listScrollController,
+          ));
         }
       },
-    ))]); */
-    return Stack(children: [
-      Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: buildConversationIcons(),
+    );
+  }
+
+  Widget buildInput() {
+    return Padding(
+      padding:
+          EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: Row(children: [
+        Flexible(
+            flex: 6,
+            child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: TextField(controller: textController))),
+        Material(
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 8.0),
+            child: IconButton(
+              icon: Icon(Icons.send),
+              onPressed: () => onSendMessage(textController.text),
+              color: orange,
             ),
           ),
-              Flexible(child: StreamBuilder(
-                stream: Firestore.instance
-                    .collection('messages')
-                    //.document(groupChatId)
-                    //.collection(groupChatId)
-                    .limit(20)
-                    .snapshots(),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return Center(
-                        child: CircularProgressIndicator(
-                            valueColor: AlwaysStoppedAnimation<Color>(orange)));
-                  } else {
-                    return ListView.builder(
-                      itemBuilder: (context, index) =>
-                          buildItem(index, snapshot.data.documents[index]),
-                      itemCount: snapshot.data.documents.length,
-                    );
-                  }
-                },
-              )),
-          /*
-        SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Column(
-              children: buildConversation(),
-            )),
-            */
-          Padding(
-            padding: EdgeInsets.only(
-                bottom: MediaQuery.of(context).viewInsets.bottom),
-            child: Row(children: [
-              Flexible(
-                  flex: 6,
-                  child: TextField(onChanged: (value) => message = value)),
-              Material(
-            child: new Container(
-              margin: new EdgeInsets.symmetric(horizontal: 8.0),
-              child: new IconButton(
-                icon: new Icon(Icons.send),
-                onPressed: () => null,
-                color: orange,
-              ),
-            ),
-            color: Colors.white,
-          ),
-            ]),
-          )
-        ],
-      )
-    ]);
+          color: Colors.white,
+        ),
+      ]),
+    );
   }
 
   Widget buildItem(int index, DocumentSnapshot document) {
-    // Right (my message)
     return Row(
       children: <Widget>[
         Container(
@@ -164,7 +171,7 @@ class _MessagingState extends State<MessagingScreen> {
           decoration: BoxDecoration(
               color: orange, borderRadius: BorderRadius.circular(8.0)),
           margin: EdgeInsets.only(bottom: 10.0, right: 10.0),
-            )
+        )
       ],
       mainAxisAlignment: MainAxisAlignment.end,
     );
