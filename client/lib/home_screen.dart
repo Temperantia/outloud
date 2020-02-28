dd0d73262e82ec5cb9608e9e221cf883aa514402
Temperance <adulorier@gmail.com>
2020-02-28 12:12:32 +0100

    new design profile

--- client/lib/home_screen.dart
+++ client/lib/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:async_redux/async_redux.dart';
 import 'package:business/app_state.dart';
 import 'package:business/permissions/location_permission.dart';
 import 'package:flutter/material.dart';
@@ -7,7 +6,7 @@ import 'package:inclusive/people/people_widget.dart';
 import 'package:inclusive/profile/profile_widget.dart';
 import 'package:inclusive/theme.dart';
 import 'package:inclusive/widgets/view.dart';
-import 'package:provider_for_redux/provider_for_redux.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String id = 'Home';
@@ -18,6 +17,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen>
     with SingleTickerProviderStateMixin, WidgetsBindingObserver {
   TabController _tabController;
+  Widget _body;
 
   @override
   void initState() {
@@ -25,10 +25,27 @@ class _HomeScreenState extends State<HomeScreen>
     WidgetsBinding.instance.addObserver(this);
     _tabController = TabController(vsync: this, length: 4);
     requestLocationPermission();
+    _body = Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(themeStyle == 'orange'
+                    ? 'images/screenPattern.png'
+                    : 'images/screenPatternPurple.png'),
+                fit: BoxFit.cover)),
+        child: TabBarView(
+            //physics: const NeverScrollableScrollPhysics(),
+            controller: _tabController,
+            children: <Widget>[
+              ProfileWidget(),
+              EventsWidget(),
+              PeopleWidget(),
+              const Center(child: Text('Coming soon'))
+            ]));
   }
 
   Future<bool> requestLocationPermission() async {
-    final bool granted = await LocationPermissionService().requestLocationPermission();
+    final bool granted =
+        await LocationPermissionService().requestLocationPermission();
     if (!granted) {
       onPermissionDenied();
     }
@@ -36,21 +53,28 @@ class _HomeScreenState extends State<HomeScreen>
   }
 
   void onPermissionDenied() {
-    showDialog(context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: const Text('PERMISSION WARNING'),
-        content: const Text('YOU SHOULD ALLOW LOCATION PERMISSION !'),
-        actions: <Widget>[
-          FlatButton(child: const Text('NO, THANKS'), onPressed: () {
-            Navigator.pop(context);
-          },),
-          FlatButton(child: const Text('GO to settings'), onPressed: () {
-            LocationPermissionService().openAppSettings();
-          },)
-        ],
-      );
-    });
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('PERMISSION WARNING'),
+            content: const Text('YOU SHOULD ALLOW LOCATION PERMISSION !'),
+            actions: <Widget>[
+              FlatButton(
+                child: const Text('NO, THANKS'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              FlatButton(
+                child: const Text('GO to settings'),
+                onPressed: () {
+                  LocationPermissionService().openAppSettings();
+                },
+              )
+            ],
+          );
+        });
   }
 
   @override
@@ -75,34 +99,21 @@ class _HomeScreenState extends State<HomeScreen>
   }
 
   Widget _buildBody() {
-    return Container(
-        decoration: const BoxDecoration(gradient: gradient),
-        child: TabBarView(
-            physics: const NeverScrollableScrollPhysics(),
-            controller: _tabController,
-            children: <Widget>[
-              ProfileWidget(),
-              EventsWidget(),
-              PeopleWidget(),
-              const Center(child: Text('Coming soon'))
-            ]));
+    return _body;
   }
 
   @override
   Widget build(BuildContext context) {
     //_authService.refreshLocation();
+    print('rebuilding home');
+    return Selector<AppState, int>(
+        selector: (BuildContext context, AppState state) => state.homePageIndex,
+        builder: (BuildContext context, int homePageIndex, Widget child) {
+          _tabController.animateTo(homePageIndex);
 
-    return ReduxConsumer<AppState>(builder: (BuildContext context,
-        Store<AppState> store,
-        AppState state,
-        void Function(ReduxAction<dynamic>) dispatch,
-        Widget child) {
-      _tabController.animateTo(state.homePageIndex);
-
-      return View(
-        child: _buildBody(),
-        showAppBar: false,
-      );
-    });
+          return View(
+            child: _buildBody(),
+          );
+        });
   }
 }
