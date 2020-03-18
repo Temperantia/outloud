22baf996b565afda93e68af0b2afb56b89afea9a
robin despouys <robin.despouys@gmail.com>
2020-03-18 20:29:56 +0100

    update chat layout in lounge

--- client/lib/lounges/lounge_chat_screen.dart
+++ client/lib/lounges/lounge_chat_screen.dart
@@ -13,6 +13,7 @@ import 'package:inclusive/theme.dart';
 import 'package:inclusive/widgets/cached_image.dart';
 import 'package:inclusive/widgets/circular_image.dart';
 import 'package:inclusive/widgets/view.dart';
+import 'package:intl/intl.dart' as date_formater;
 import 'package:provider_for_redux/provider_for_redux.dart';
 
 class LoungeChatScreen extends StatefulWidget {
@@ -120,14 +121,98 @@ class _LoungeChatScreenState extends State<LoungeChatScreen> {
 
   Widget _buildChat(Chat chat) {
     return ListView.builder(
-        reverse: true,
+        reverse: false,
         itemCount: chat.messages.length,
         itemBuilder: (BuildContext context, int index) =>
-            _buildMessage(chat.messages[index]));
+            _buildMessage(chat.messages[chat.messages.length - index - 1]));
+  }
+
+  String _dateFormatter(int timestamp) {
+    final DateTime time = DateTime.fromMillisecondsSinceEpoch(timestamp);
+    final int daysDifference = DateTime.now().difference(time).inDays;
+    if (daysDifference < 7) {
+      return date_formater.DateFormat(' E \'at\' kk:mm').format(time);
+    }
+    return date_formater.DateFormat('yyyy-MM-dd \'at\' kk:mm').format(time);
   }
 
   Widget _buildMessage(Message message) {
-    return Text(message.content);
+    final User user = widget.lounge.members.firstWhere((User user) {
+      return user.id == message.idFrom;
+    });
+    return Container(
+        padding: const EdgeInsets.all(4),
+        margin: const EdgeInsets.all(4),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          textDirection: widget.lounge.owner == user.id
+              ? TextDirection.rtl
+              : TextDirection.ltr,
+          children: <Widget>[
+            Container(
+                padding: const EdgeInsets.all(5),
+                child: CircularImage(
+                  imageUrl: user.pics.isNotEmpty ? user.pics[0] : null,
+                  imageRadius: 35.0,
+                )),
+            Expanded(
+                child: Container(
+                    decoration: BoxDecoration(
+                        color: pinkLight.withOpacity(
+                            widget.lounge.owner == user.id ? 0.7 : 0.4),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(5))),
+                    padding: const EdgeInsets.only(
+                        left: 10.0, right: 10.0, top: 10, bottom: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                            child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Container(
+                                child: RichText(
+                              text: TextSpan(
+                                text: widget.lounge.members
+                                    .firstWhere((User user) {
+                                  return user.id == message.idFrom;
+                                }).name,
+                                style: const TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                            )),
+                            Container(
+                                child: RichText(
+                              text: TextSpan(
+                                text: _dateFormatter(message.timestamp),
+                                style: const TextStyle(
+                                    color: Colors.black38,
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w400),
+                              ),
+                            )),
+                          ],
+                        )),
+                        Container(
+                            padding: const EdgeInsets.only(top: 5),
+                            child: RichText(
+                              textAlign: TextAlign.left,
+                              text: TextSpan(
+                                text: message.content,
+                                style: const TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w400),
+                              ),
+                            ))
+                      ],
+                    )))
+          ],
+        ));
   }
 
   @override
@@ -152,7 +237,7 @@ class _LoungeChatScreenState extends State<LoungeChatScreen> {
                         children: <Widget>[
                           _buildHeader(state, dispatch),
                           const Divider(),
-                          Expanded(child: _buildChat(chat))
+                          Expanded(child: Container(child: _buildChat(chat)))
                         ],
                       )))),
               Row(
